755326eec24134a005eafd58c2fd85feec1eced3
BenedictRM <mehringr@gmail.com>
2018-03-28 21:45:52 -0600

    saving updates to proj

--- courseProject/src/app/app.module.ts
+++ courseProject/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipesComponent } from './recipe-book/recipes/recipes.component';
 import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { RecipesComponent } from './recipe-book/recipes/recipes.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,7 @@ import { RecipesComponent } from './recipe-book/recipes/recipes.component';
     HeaderComponent,
     RecipeListComponent,
     RecipeDetailComponent,
-    RecipesComponent,
+    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
     RecipesComponent
--- courseProject/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ courseProject/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'Simply a Test', 'https://miami.eat24hours.com/files/cuisines/v4/thai.jpg')
+  ];
 
   constructor() { }
 
