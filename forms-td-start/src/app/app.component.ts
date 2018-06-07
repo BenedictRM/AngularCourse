969e6a11295126380f16299056c5489004895056
BenedictRM <mehringr@gmail.com>
2018-06-06 22:28:19 -0600

    wrapping up template driven forms

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -9,9 +9,38 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
 
   @ViewChild('f') signupForm : NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secret: '',
+    answer: '',
+    gender: ''
+  }
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    console.log('called');
+    //Example to overwrite entire form
+    // this.signupForm.setValue({
+    //   userGroup: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+
+    //patchValue() example
+    this.signupForm.form.patchValue({
+      userGroup: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm){
@@ -20,6 +49,14 @@ export class AppComponent {
 
   onSubmit(){
     console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userGroup.username;
+    this.user.email = this.signupForm.value.userGroup.email;
+    this.user.secret = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 
 }
